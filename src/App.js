31e36edd4b9d93f9a3562892530adea8c5723967
rpsingh6634 <rpsingh6634@gmail.com>
2022-11-22 18:38:38 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
-function App() {
+//☀︎ ☽
+export default function App() {
+  const [darkMode, setDarkMode] = useState(false);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={darkMode ? "dark-mode" : "light-mode"}>
+      <div className="container">
+      <h2>Overreacted</h2>
+      &emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;
+        <span style={{ color: darkMode ? "grey" : "yellow" }}>☀︎</span>
+        <div className="switch-checkbox">
+          <label className="switch">
+            <input type="checkbox" onChange={() => setDarkMode(!darkMode)} />
+            <span className="slider round"> </span>
+          </label>
+        </div>
+        <span style={{ color: darkMode ? "#c96dfd" : "grey" }}>☽</span>
+      </div>
+      <div>
+        <div className="content1">
+        <h2 className="wet">The Wet Codbase</h2>
+        <p>Lorem ipsum dolor sit,consectetur</p>
+        <br/>
+        <p>Lorem ipsum dolor sit,consectetur</p>
+        <br/>
+        <br/>
+        <h2 className="wet">Goodby, Clean Code</h2>
+        <p>Lorem ipsum dolor sit,consectetur</p>
+        <br/>
+        <p>Lorem ipsum dolor sit,consectetur</p>
+        <br/>
+        <br/>
+        <h2 className="wet">My Decade In Review</h2>
+        <p>Lorem ipsum dolor sit,consectetur</p>
+        <br/>
+        <p>Lorem ipsum dolor sit,consectetur</p>
+        <br/>
+        <br/>
+        <h2 className="wet">What are the react Team Principles</h2>
+        <p>Lorem ipsum dolor sit,consectetur</p>
+        </div>
+      </div>
     </div>
   );
 }
-
-export default App;
